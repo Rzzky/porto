f7035f45ec74674a0cfcaeae4dbbeca6bc6709ca
Rzzky <88342236+Rzzky@users.noreply.github.com>
2024-03-27 11:50:27 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,101 @@
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 120;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
+
+// THE CODE BELOW IS FOR IMAGE MAGNIFY
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = "assets/1Serti.jpg";
+  modalImg.alt = this.alt;
+  captionText.innerHTML = this.alt;
+};
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function () {
+  img01.className += " out";
+  setTimeout(function () {
+    modal.style.display = "none";
+    img01.className = "modal-content";
+  }, 400);
+};
+
+var img = document.getElementById("myImg1");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = "assets/2Serti.jpg";
+  modalImg.alt = this.alt;
+  captionText.innerHTML = this.alt;
+};
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function () {
+  img01.className += " out";
+  setTimeout(function () {
+    modal.style.display = "none";
+    img01.className = "modal-content";
+  }, 400);
+};
+
+var img = document.getElementById("myImg2");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = "assets/3Serti.jpg";
+  modalImg.alt = this.alt;
+  captionText.innerHTML = this.alt;
+};
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function () {
+  img01.className += " out";
+  setTimeout(function () {
+    modal.style.display = "none";
+    img01.className = "modal-content";
+  }, 400);
+};
+
+var img = document.getElementById("myImg3");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = "assets/4Serti.jpg";
+  modalImg.alt = this.alt;
+  captionText.innerHTML = this.alt;
+};
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function () {
+  img01.className += " out";
+  setTimeout(function () {
+    modal.style.display = "none";
+    img01.className = "modal-content";
+  }, 400);
+};
